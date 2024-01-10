742cbd1b04f6258272ab100846036a4ff7bf111a
Arjun Krishna <arjun12345krishna@gmail.com>
2024-01-10 12:39:22 +0530

    Update update_readme.js

--- scripts/update_readme.js
+++ scripts/update_readme.js
@@ -4,6 +4,8 @@ function updateReadme() {
     const readmePath = './README.md';
     const readmeContent = fs.readFileSync(readmePath, 'utf-8');
 
+    console.log(readmeContent);
+
     // Modify the README content as needed
     const updatedContent = readmeContent + "\n\nThis line was added by the GitHub Action.";
 
